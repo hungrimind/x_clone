3bf144210d59dad22d3eec7217eded51d614fe04
Tadas Petra <tadas@tadaspetra.com>
2025-01-05 10:30:54 -0600

    update lessons to default to Home View

--- steps/tabbar/lib/main.dart
+++ steps/tabbar/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:demo/pages/signin.dart';
+import 'package:demo/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const SignIn(),
+      home: const Home(),
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.black,
