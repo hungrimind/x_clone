52e4794fbcb1ad3f85a2184feff72d40376fab43
Tadas Petra <tadas@tadaspetra.com>
2025-01-05 11:31:04 -0600

    another post

--- steps/eachpost/lib/models/post.dart
+++ steps/eachpost/lib/models/post.dart
@@ -37,5 +37,19 @@ class Post {
       profilePictureUrl:
           'https://pbs.twimg.com/profile_images/1498070100393754625/C2V-fbll_400x400.jpg',
     ),
+    Post(
+      name: 'Michael Daly',
+      handle: '@drmikeDO1943',
+      verified: true,
+      time: '3h',
+      content:
+          "A grandson just texted: How is X? Why do you think it's newsworthy? My answer: It is panoply of views/comments and it is continuously updated. You immerse yourself in the 'town square' of discussion and come to your own conclusions about what is true.",
+      comments: '3.2K',
+      reposts: '454',
+      likes: '113K',
+      views: '14M',
+      profilePictureUrl:
+          'https://pbs.twimg.com/profile_images/1585715412116930561/CHkTz-aM_400x400.jpg',
+    ),
   ];
 }
