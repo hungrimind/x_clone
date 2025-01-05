385a117f4caafe904a73b7895e3e6f626b743769
Tadas Petra <tadas@tadaspetra.com>
2025-01-05 09:02:50 -0600

    Add redirect to home after navigate

--- steps/navigate/lib/main.dart
+++ steps/navigate/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:demo/pages/home.dart';
 import 'package:flutter/material.dart';
+
+import '/pages/signin.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Home(),
+      home: const SignIn(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
