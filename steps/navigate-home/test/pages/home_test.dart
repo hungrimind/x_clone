afde7e98c407b5dbabfa5d32e8b5b819e1d32605
Robert Brunhage <robertbrunhage@gmail.com>
2025-02-12 08:37:26 +0100

    chore: format all steps

--- steps/navigate-home/test/pages/home_test.dart
+++ steps/navigate-home/test/pages/home_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('_', (WidgetTester tester) async { });
+  testWidgets('_', (WidgetTester tester) async {});
 }
--- steps/start/test/pages/signin_test.dart
+++ steps/start/test/pages/signin_test.dart
@@ -1,9 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
   testWidgets('TabBarView should display correct content for each tab',
-      (WidgetTester tester) async {
-  
-  });
+      (WidgetTester tester) async {});
 }
